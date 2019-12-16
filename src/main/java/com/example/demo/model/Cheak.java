90254913cd8224499640423eacf51a522430f2f0
20fen <15622969639@163.com>
2019-12-16 17:46:25 +0800

    提交

--- src/main/java/com/example/demo/model/Cheak.java
+++ src/main/java/com/example/demo/model/Cheak.java
@@ -1,14 +1,29 @@
 package com.example.demo.model;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiOperation;
 import lombok.Data;
+import lombok.experimental.Accessors;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 
 /**
  * Description: 实体类
  */
 @Data
+@ApiModel(value = "请求实体")
+//lombok @Accessors生成  set
+@Accessors(chain = true)
 public class Cheak {
 
+   @ApiModelProperty(name = "手机号")
+   @Pattern(regexp = "",message = "手机号不正确")
+   @NotBlank(message = "手机号不能为空")
    private String phone;
+   @ApiModelProperty(name = "验证码")
+   @NotBlank(message = "验证码不能为空")
    private String code;
 }
