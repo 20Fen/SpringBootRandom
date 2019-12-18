d569f2c04b3f0c5b50f990fe789a946e22aca1e7
20fen <15622969639@163.com>
2019-12-18 18:28:30 +0800

    提交

--- src/main/java/com/example/demo/model/Cheak.java
+++ src/main/java/com/example/demo/model/Cheak.java
@@ -19,7 +19,7 @@ import javax.validation.constraints.Pattern;
 public class Cheak extends BaseProtocolIn{
 
    @ApiModelProperty(name = "手机号")
-   @Pattern(regexp = "^((13[0-9])|(14[5|7])|(15([0-3]|[5-9]))|(17[013678])|(18[0,5-9]))\\d{8}$",message = "手机号不正确")
+   @Pattern(regexp = "^[1](([3][0-9])|([4][5-9])|([5][0-3,5-9])|([6][5,6])|([7][0-8])|([8][0-9])|([9][1,8,9]))[0-9]{8}$",message = "手机号格式不正确")
    @NotBlank(message = "手机号不能为空")
    private String phone;
    @ApiModelProperty(name = "验证码")
--- src/main/java/com/example/demo/model/Phones.java
+++ src/main/java/com/example/demo/model/Phones.java
@@ -15,7 +15,7 @@ import javax.validation.constraints.Pattern;
 public class Phones extends BaseProtocolIn{
 
     @ApiModelProperty(name = "手机号")
-    @Pattern(regexp = "^[1](([3][0-9])|([4][5-9])|([5][0-3,5-9])|([6][5,6])|([7][0-8])|([8][0-9])|([9][1,8,9]))[0-9]{8}$",message = "手机号不正确")
+    @Pattern(regexp = "^[1](([3][0-9])|([4][5-9])|([5][0-3,5-9])|([6][5,6])|([7][0-8])|([8][0-9])|([9][1,8,9]))[0-9]{8}$",message = "手机号格式不正确")
     @NotBlank(message = "手机号不能为空")
     private String phone;
 }
